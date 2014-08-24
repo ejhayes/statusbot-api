6693b97b8a0c57fc53495b72e5234d854c3a7dc9
Eric Hayes <eric@deployfx.com>
2014-08-24 10:55:27 -0700

    Added support for activerecord model_mock and model_stub.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,12 @@ require 'database_cleaner'
 require 'statusbot/api'
 require 'debugger'
 require 'securerandom'
+require 'rspec/rails/mocks'
 
 RSpec.configure do |config|
+  
+  config.include RSpec::Rails::Mocks
+  
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
--- statusbot-api.gemspec
+++ statusbot-api.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rspec", "~> 2.14.1"
+  spec.add_development_dependency 'rspec-rails'
   spec.add_development_dependency "debugger"
   spec.add_development_dependency 'simplecov', '~> 0.7.1'
   spec.add_development_dependency 'database_cleaner'
